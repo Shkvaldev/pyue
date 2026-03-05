dec5a260ffedfddd635668b71738c2bcc2880371
ZaShkval <shkvalgrozny@gmail.com>
2026-03-05 23:21:30 +0500

    added backend interface

---
+++ pyue/core/app.py
@@ -0,0 +1,6 @@
+from pyue.core.backend import Backend
+
+class App:
+    """Master of web app"""
+    def __init__(self, backend: Backend) -> None:
+        pass
--- pyue/core/backend.py
+++ pyue/core/backend.py
@@ -1,5 +1,18 @@
-from abc import ABC
+from typing import Any
+from abc import ABC, abstractmethod
+
+from pyue.core.page import Page
 
 
 class Backend(ABC):
     """Represents http server for"""
+
+    @abstractmethod
+    def add_page(self, page: Page, *args, **kwargs):
+        """Includes page into router (Blueprint for Flask, APIRouter for FastAPI) into app"""
+        pass
+
+    @abstractmethod
+    def include_static(self, path: str, url: str, *args, **kwargs):
+        """Includes static files"""
+        pass
